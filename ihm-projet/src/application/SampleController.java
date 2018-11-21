22c78e39b584ff109df48fcc837c1e90f7f5e741
aaliberato <aaliberato@scinfe054.univ-pau.fr>
2018-11-21 09:05:20 +0100

    Init environement

---
+++ ihm-projet/src/application/SampleController.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+ 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SampleController {
+	
+	 
+}
