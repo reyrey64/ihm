b2be90f1fad0a24ceacf76abf31a680e91ea5867
sbhazim001 <sbhazim001@scinfe053.univ-pau.fr>
2018-11-28 09:24:06 +0100

    Page d'accueil avec liste deroulante des langues dispos + changement style

--- ihm-projet/src/application/Main.java
+++ ihm-projet/src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -11,8 +12,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Accueil.fxml"));
-			Scene scene = new Scene(root,400,400);
+			
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Accueil.fxml"));
+			Scene scene = new Scene(root,550,650);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
