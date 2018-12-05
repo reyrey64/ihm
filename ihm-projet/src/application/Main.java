6f63f982fd1e49696b103821e31400fe80ac9122
sbhazim001 <sbhazim001@scinfe053.univ-pau.fr>
2018-12-05 08:25:38 +0100

    Ajout page exo, magazine

--- ihm-projet/src/application/Main.java
+++ ihm-projet/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			
-			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Accueil.fxml"));
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Inscription.fxml"));
 			Scene scene = new Scene(root,550,650);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
